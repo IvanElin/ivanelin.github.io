61268a8a942b0ac5c46f17bf5001fc87e953f3e8
IvanElin <ivan.nero1@gmail.com>
2018-02-16 11:28:49 +0200

    SmoothScrolling added

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Smooth Scrolling
+$(document).ready(function(){
+  var scrollLink = $('.scroll');
+
+  scrollLink.click(function(e) {
+    var linkHref = $(this).attr('href');
+    // console.log($(linkHref).offset().top);
+
+    $('html, body').animate({
+      scrollTop: $(linkHref).offset().top
+    });
+    
+    e.preventDefault();
+
+  });
+})
